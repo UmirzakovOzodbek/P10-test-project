c98829bcf8e9fb7e3aefece01ce979e2f16b80a4
user <ozodbekumurzakov2006@gmail.com>
2023-01-27 16:29:04 +0500

    .gitignore updated

--- _3m_lesson3/exercise3.py
+++ _3m_lesson3/exercise3.py
@@ -1,11 +1,16 @@
-def multiplication_two_number(a, b):
-    def multiplication_inner():
-        return f"({a} + {a}) + ({b} + {b}) = {(a + a) + (b + b)}"
+def multiplication_two_number(func):
+    def multiplication_inner(a, b):
+        return f"({a} + {a}) + ({b} + {b}) = {(a + b) * 2}"
 
     return multiplication_inner
 
 
-multiplication = multiplication_two_number(2, 3)
-multiplication1 = multiplication_two_number(5, 5)
-print(multiplication())
-print(multiplication1())
+@multiplication_two_number
+def decoration(a, b):
+    return (a + b) * 2
+
+
+print(decoration(2, 3))
+print(decoration(5, 5))
+
+
