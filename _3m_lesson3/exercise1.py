169dc8ecfd3b3262281342ccb6935d3b848b90e9
user <ozodbekumurzakov2006@gmail.com>
2023-01-27 15:39:23 +0500

    .gitignore updated

--- _3m_lesson3/exercise1.py
+++ _3m_lesson3/exercise1.py
@@ -1,14 +1,11 @@
 def get_next_multiple(nums):
-    for num in nums:
-        yield num + num
-        yield num + num + num
-        yield num + num + num + num
-        yield num + num + num + num + num
-        yield num + num + num + num + num + num
+    num = nums
+    while True:
+        yield nums
+        nums += num
 
 
-numbers = [13]
-gen_multiple_of_two = get_next_multiple(numbers)
+gen_multiple_of_two = get_next_multiple(2)
 print(next(gen_multiple_of_two))
 print(next(gen_multiple_of_two))
 print(next(gen_multiple_of_two))
